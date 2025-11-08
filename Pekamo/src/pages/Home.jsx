4f9e2003fc108115289302b22ab2080e1c882546
Stevekamoni <stevekamoni4@gmail.com>
2025-11-08 23:55:21 +0300

    Added info tabs but still need adjustments

--- Pekamo/src/pages/Home.jsx
+++ Pekamo/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import InfoTabs from '../components/InfoTabs'
+
 
 const Home = () => {
   return (
     <div>
         <Hero/>
+        <InfoTabs/>
     </div>
   )
 }
