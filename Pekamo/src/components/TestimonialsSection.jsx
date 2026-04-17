d77d4a9d75d1080b87e29e5bfb25d8994e826137
Stevekamoni <stevekamoni4@gmail.com>
2026-04-17 11:50:06 +0300

    Build error 3

--- Pekamo/src/components/TestimonialsSection.jsx
+++ Pekamo/src/components/TestimonialsSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../styles/testimonialsSection.module.scss";
+import styles from "../styles/TestimonialsSection.module.scss";
 import { motion } from "framer-motion";
 
 const Testimonials = () => {
